12965ca3448c585e230bff7be0254001a68e1fbb
Haroldi3 <hinir001@fiu.edu>
2025-07-25 20:44:50 -0400

    Update modelsWorkout.js

--- modelsWorkout.js
+++ modelsWorkout.js
@@ -1,1 +1,30 @@
-nul not found
+const mongoose = require('mongoose');
+
+const WorkoutSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  sets: {
+    type: Number,
+    required: true,
+  },
+  reps: {
+    type: Number,
+    required: true,
+  },
+  weight: {
+    type: Number,
+    required: true,
+  },
+  rpe: {
+    type: Number,
+    required: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+module.exports = mongoose.model('Workout', WorkoutSchema);
